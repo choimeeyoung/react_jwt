035e5f091cd365b9b95e04c3f7de22c8118cd859
choimy8018 <choimy8018@bahnb.com>
2021-05-27 19:06:16 +0900

    update : front 주소 수정2

--- back/app.js
+++ back/app.js
@@ -35,7 +35,7 @@ if(process.env.NODE_ENV ==='production'){                       // 배포 모드
 }
 
 app.use(cors({
-    origin:['http://localhost:3000' , '54.180.92.229'],                   // 추후 우리의 사이트 주소로 수정
+    origin:['http://localhost:3000' , 'http://54.180.92.229'],                   // 추후 우리의 사이트 주소로 수정
     credentials:true                                     // cookies 값을 Front 와 같이 공유 하려고 할때 사용
 }));
 
