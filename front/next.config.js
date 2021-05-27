25d19c6593728e90d6f17a9c5bde5c0caf173388
choimy8018 <choimy8018@bahnb.com>
2021-05-27 14:23:33 +0900

    update : next.config.js devtool 'inline-source-map' 수정

--- front/next.config.js
+++ front/next.config.js
@@ -7,16 +7,20 @@ module.exports = withBundleAnalyser({
     future :{
       webpack5:true
     },
-    webpack(config,{webpack}){
+    webpack(config,{webpack,dev}){
         const prod = process.env.NODE_ENV === 'production';
         const plugins = [...config.plugins];
         if(prod){
             plugins.push(new CompressPlugin());
         }
+
+        if (dev) {
+            config.devtool = 'inline-source-map';
+        }
         return {
             ...config,
             mode:prod ? 'production':'development',
-            devtool: prod ? 'hidden-source-map' : 'eval',
+            devtool: prod ? 'hidden-source-map' : 'inline-source-map',
             plugins
         }
     }
