e853a08d2ad7e89cfaa8f27c372329aa794fe79d
choimy8018 <choimy8018@bahnb.com>
2021-05-29 20:36:49 +0900

    Test

--- back/app.js
+++ back/app.js
@@ -57,7 +57,7 @@ app.use(session({
     cookie: {
         httpOnly: true,
         secure: false,
-        domain: process.env.NODE_ENV === 'production' && '.cmyreact.com'
+        domain: 'http://cmyreact.com'
     },
 }))
 
